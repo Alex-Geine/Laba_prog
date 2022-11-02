beb3b25ca0d8b87af029615a7c23941349f2ad66
mrbor <mrbor@DESKTOP-BOETFH8>
2022-11-02 23:01:30 +0300

    Добавьте файлы проекта.

---
+++ Laba_prog/Laba_prog.h
@@ -0,0 +1,32 @@
+﻿
+// Laba_prog.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CLabaprogApp:
+// Сведения о реализации этого класса: Laba_prog.cpp
+//
+
+class CLabaprogApp : public CWinApp
+{
+public:
+	CLabaprogApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CLabaprogApp theApp;
---
+++ Laba_prog/Laba_progDlg.h
@@ -0,0 +1,42 @@
+﻿
+// Laba_progDlg.h: файл заголовка
+//
+
+#pragma once
+#include "Drawer.h"
+
+// Диалоговое окно CLabaprogDlg
+class CLabaprogDlg : public CDialogEx
+{
+// Создание
+public:
+	CLabaprogDlg(CWnd* pParent = nullptr);	// стандартный конструктор
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_LABA_PROG_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// поддержка DDX/DDV
+
+
+// Реализация
+protected:
+	HICON m_hIcon;
+
+	// Созданные функции схемы сообщений
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	//Обьекты для графиков
+	Drawer
+		Graph_Sign, //сигнал на входе
+		Graph_ACH,	//АЧХ
+		Graph_FCH;	//ФЧХ
+
+};
